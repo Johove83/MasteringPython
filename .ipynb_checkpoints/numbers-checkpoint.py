205e43f7952c2f7aafb48154829b9eb9c1e575ad
unknown <johnathan.overton83@gmail.com>
2022-07-08 15:15:56 -0600

    Pushing practice files to the repository.

---
+++ .ipynb_checkpoints/numbers-checkpoint.py
@@ -0,0 +1,17 @@
+#Let's play with some numbers
+
+name = "Johnathan"
+
+age = 38
+
+actual_age = 38.625
+
+math = 5 ** 7 + 6 * 7 - 4
+
+print(math)
+
+results = age + actual_age + math
+
+print(results)
+
+print(type(results))
---
+++ variables.py
@@ -0,0 +1,23 @@
+#Let's start a coffee shop together!! We're going to build a coffee shop using some new Python programming concepts!!
+
+#Let's build robot Barista!!
+
+print("\nHello, welcome to NetworkJohn Coffee!!!!!!!\n")
+
+name = input("What is your name?\n\n")
+
+print("\nHello " + name + ", thank you so much for coming in today!!\n\nHere is a menu " + name + ".\n")
+
+menu = "Black Coffee, Espresso, Latte, Cappucino"
+
+print("\n" + menu)
+
+order = input()
+
+price = 8
+
+quantity = input("\nHow many " + menu + " would you like?\n")
+
+total = price * int(quantity)
+
+print("Okay " + name + ", your total is " + str(total) + " dollars.")
