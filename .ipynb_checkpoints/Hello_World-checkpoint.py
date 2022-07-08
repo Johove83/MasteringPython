569fd4a89a9d7d86828150e37e3f73de99261d83
unknown <johnathan.overton83@gmail.com>
2022-07-08 13:52:35 -0600

    Let's go

---
+++ .ipynb_checkpoints/Hello_World-checkpoint.py
@@ -0,0 +1,13 @@
+#hey....there is nothing here. WAKE UP!!
+
+#print("Hello World!!!!!!")
+#print('I am Iron Man')
+#print("No, I am Tony Stark")
+#print(No, I am Poppy)
+#We are practicing Python....IT'S AWESOME!!!
+#
+
+print("I am Iron Man. " + "\nNo, I am Tony Stark. " + "\nNo, I am Poppy")
+
+
+print("I am Poppy\n" * 100)
