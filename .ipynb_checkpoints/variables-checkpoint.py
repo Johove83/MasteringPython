ed483aed26f8de0c7ce33b0dae8a733e2db5a806
unknown <johnathan.overton83@gmail.com>
2022-07-10 20:10:41 -0600

    Don't lose it.

--- .ipynb_checkpoints/variables-checkpoint.py
+++ .ipynb_checkpoints/variables-checkpoint.py
@@ -8,16 +8,47 @@ name = input("What is your name?\n\n")
 
 print("\nHello " + name + ", thank you so much for coming in today!!\n\nHere is a menu " + name + ".\n")
 
-menu = "Black Coffee, Espresso, Latte, Cappucino"
+menu = "Black Coffee, Espresso, Latte, Cappucino, Frappuccino"
 
 print("\n" + menu)
 
 order = input()
+price = 0
+if order == "Frappuccino":
+    price = 13
+    quantity = input("How many " + order + " would you like, " + name + "?\n\n")
+    total = price * int(quantity)
+    print("Okay " + name + ", your total is $" + str(total) + " dollars.\n\nWe will have that " + str(quantity) + " " + order + " out for you shortly.")
+elif order == "Black Coffee":
+    price = 3
+    quantity = input("How many " + order + " would you like, " + name + "?\n\n")
+    total = price * int(quantity)
+    print("Okay " + name + ", your total is $" + str(total) + " dollars.\n\nWe will have that " + str(quantity) + " " + order + " out for you shortly.")
+elif order == "Espresso":
+    price = 5
+    quantity = input("How many " + order + " would you like, " + name + "?\n\n")
+    total = price * int(quantity)
+    print("Okay " + name + ", your total is $" + str(total) + " dollars.\n\nWe will have that " + str(quantity) + " " + order + " out for you shortly.")
+elif order == "Latte":
+    quantity = input("How many " + order + " would you like, " + name + "?\n\n")
+    cream = input("\nWould you like whipped cream with that Latte, " + name + "?\n\n")
+    if cream == "Yes":
+        price = 11
+        total = price * int(quantity)
+        print("Okay " + name + ", your total is $" + str(total) + " dollars.\n\nWe will have that " + str(quantity) + " " + order + " out for you shortly.")
+    else:
+        price = 9
+        total = price * int(quantity)
+        print("Okay " + name + ", your total is $" + str(total) + " dollars.\n\nWe will have that " + str(quantity) + " " + order + " out for you shortly.")
+elif price == "Cappucino":
+    price = 10
+    quantity = input("How many " + order + " would you like, " + name + "?\n\n")
+    total = price * int(quantity)
+    print("Okay " + name + ", your total is $" + str(total) + " dollars.\n\nWe will have that " + str(quantity) + " " + order + " out for you shortly.")
+else:
+    print("Sorry, that is not on our menu.")
 
-price = 8
 
-quantity = input("\nHow many " + menu + " would you like?\n")
+    
 
-total = price * int(quantity)
 
-print("Okay " + name + ", your total is $" + str(total) + " dollars.\n\nWe will have that " + str(quantity) + " " + order + " out for you shortly.")
---
+++ unwelcome_ben.py
@@ -0,0 +1,15 @@
+print("Hello, welcome to Johove's Coffee Shop!!!!")
+
+name = input("\nWhat is your name?\n\n")
+
+if name == "Kassie":
+    evil_status = input("Are you evil?\n")
+    if evil_status == "Yes":
+        print("\nYou're not welcome here Kassie!! Get out!!")
+    else:
+        print("\nOh wow!!! A good Kassie!! Come on in!")
+else:
+    print("\nHello " + name + ", thank you so much for coming in today.")
+
+
+
